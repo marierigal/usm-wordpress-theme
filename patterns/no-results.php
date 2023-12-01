8a372267f6b4e545f16f4eb3299f5ddbf605d0e4
Marie Rigal <marierigal4@gmail.com>
2023-12-01 17:59:33 +0100

    feat(templates): add index template

---
+++ patterns/no-results.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Query No Results
+ * Slug: usm/no-results
+ * Categories: theme, posts
+ */
+?>
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|main"}}}},"backgroundColor":"tertiary","textColor":"main"} -->
+<p
+  class="has-text-align-center has-main-color has-tertiary-background-color has-text-color has-background has-link-color"
+>
+  <?php _e('No posts found.', DOMAIN); ?>
+</p>
+<!-- /wp:paragraph -->
---
+++ patterns/vertical-post.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: Vertical Post
+ * Slug: usm/vertical-post
+ * Categories: posts
+ */
+?>
+
+<!-- wp:group {"tagName":"article","layout":{"type":"default"}} -->
+<article class="wp-block-group">
+  <!-- wp:post-featured-image {"isLink":true,"aspectRatio":"1","style":{"color":{"duotone":"var:preset|duotone|duotone-punchy"},"spacing":{"margin":{"top":"0","bottom":"0"}}}} /-->
+
+  <!-- wp:post-title {"level":3,"isLink":true,"style":{"spacing":{"margin":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"fontSize":"x-large"} /-->
+
+  <!-- wp:post-date {"format":"j M Y","style":{"elements":{"link":{"color":{"text":"var:preset|color|gray-dark"}}},"typography":{"textTransform":"uppercase"}},"textColor":"gray-dark","fontSize":"small"} /-->
+
+  <!-- wp:post-excerpt {"moreText":"Lire la suite →"} /-->
+</article>
+<!-- /wp:group -->
