07e4ae0ff9d96bb2a2aeaa11c93223f72e5ca0dc
Marie Rigal <marierigal4@gmail.com>
2023-12-04 20:15:16 +0100

    feat(templates): add 404 template

---
+++ patterns/404.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Title: 404
+ * Slug: usm/404
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+
+<!-- wp:image {"lightbox":{"enabled":false},"id":1705,"sizeSlug":"full","linkDestination":"none","style":{"color":[]},"className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default">
+  <img
+    class="wp-image-1705"
+    src="<?php echo ASSETS . '/images/404-illustration.png'; ?>"
+    alt=""
+  />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="wp-block-heading has-text-align-center">
+  <?php _e('404 Error', DOMAIN); ?>
+</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">
+  <?php _e('The page you are looking for does no longer exists or has been moved.', DOMAIN); ?>
+</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons">
+  <!-- wp:button -->
+  <div class="wp-block-button">
+    <a class="wp-block-button__link wp-element-button" href="<?php echo get_home_url(); ?>">
+      <?php _e('Back home', DOMAIN); ?>
+    </a>
+  </div>
+  <!-- /wp:button -->
+</div>
+<!-- /wp:buttons -->
