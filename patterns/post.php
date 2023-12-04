1fb9f506a16e69bfa57206816cc4f2049027b331
Marie Rigal <marierigal4@gmail.com>
2023-12-04 20:16:42 +0100

    feat(templates): add post templates

---
+++ patterns/post.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Full Post
+ * Slug: usm/post
+ * Categories: posts
+ */
+?>
+
+<!-- wp:post-featured-image {"height":"400px","style":{"color":{"duotone":"unset"}}} /-->
+
+<!-- wp:post-title {"level":1} /-->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","verticalAlignment":"bottom"}} -->
+<div class="wp-block-group">
+  <!-- wp:post-date {"style":{"elements":{"link":{"color":{"text":"var:preset|color|gray-dark"}}},"typography":{"textTransform":"uppercase"}},"textColor":"gray-dark","fontSize":"small"} /-->
+
+  <!-- wp:post-terms {"term":"category"} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:post-content /-->
